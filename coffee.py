e26b75c5c307a72a8199187a68501061baee2cbb
Myriam.RdzReyna <myjrdz008@gmail.com>
2026-04-19 14:55:43 -0600

    Se añadio los getters

---
+++ coffee.py
@@ -0,0 +1,27 @@
+# Clase
+class Coffee:
+    def __init__(self, nombre, tipo_grano, ID, tipo_temperatura, precio):
+        self.__nombre = nombre
+        self.__tipo_grano = tipo_grano
+        self.__ID = ID
+        self.__tipo_temperatura = tipo_temperatura
+        self.__precio = precio
+
+# Getters
+def get_nombre(self):
+        return self.__nombre
+
+def get_tipo_grano(self):
+        return self.__tipo_grano
+
+def get_ID(self):
+        return self.__ID
+
+def get_tipo_temperatura(self):
+        return self.__tipo_temperatura
+
+def get_precio(self):
+        return self.__precio
+
+# Setters
+
